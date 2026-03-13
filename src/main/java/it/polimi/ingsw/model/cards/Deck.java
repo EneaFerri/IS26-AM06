501ad323193162cbf624269a96651a44dafd3490
Enea Ferri <enea.ferri@mail.polimi.it>
2026-03-13 11:18:55 +0100

    classi model senza metodi

--- src/main/java/it/polimi/ingsw/model/cards/Deck.java
+++ src/main/java/it/polimi/ingsw/model/cards/Deck.java
@@ -1,8 +1,47 @@
 package it.polimi.ingsw.model.cards;
 
-import java.util.ArrayList;
+import it.polimi.ingsw.model.enums.Age;
+
+import java.util.*;
+
+
+import static java.util.Collection.*;
 
 public class Deck
 {
-    ArrayList<Card> cards;
+    private List<Card> cards;
+    private List<TribeCard> tribeCards;
+    private List<BuildingCard> buildingCards;
+
+
+    public Deck(){
+        tribeCards = new ArrayList<>();
+        buildingCards = new ArrayList<>();
+
+    }
+
+    public void addCard(TribeCard c){
+        tribeCards.add(c);
+    }
+
+    public Card draw(){
+        if(tribeCards.isEmpty())
+            return null;
+        return tribeCards.remove(0);
+    }
+
+    public void randomize (){
+        Collections.shuffle(tribeCards);
+
+    }
+
+    public boolean isEmpty(){
+        return tribeCards.isEmpty();
+    }
+
+    public int size(){
+        return tribeCards.size();
+    }
+
+    //TODO: metodo create deck per age?
 }
