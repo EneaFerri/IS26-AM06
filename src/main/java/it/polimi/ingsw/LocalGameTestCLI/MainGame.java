91b8c5237c298471e115ea46866b64170d95ac13
Enea Ferri <enea.ferri@mail.polimi.it>
2026-04-19 17:15:56 +0200

    aggiunte alcune note per implementare client server in classe MainGame

--- src/main/java/it/polimi/ingsw/LocalGameTestCLI/MainGame.java
+++ src/main/java/it/polimi/ingsw/LocalGameTestCLI/MainGame.java
@@ -16,70 +16,79 @@ import it.polimi.ingsw.model.board.BoardSpace;
 
 public class MainGame {
 
-    //Classe per testare il gioco in locale da terminale!!!!!!!!!!!!!!!!!
+    //Classe per testare il gioco in locale da terminale --> i commenti con freccia indicano appunti sulle chiamate in client/server
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Game game = new Game(0);
+        Game game = new Game(0); // --> server fa partire il gioco con id casuale quano parte programma
 
         System.out.println("GAME STATUS: " + game.getStatus());
 
         int numberOfPlayersInt=5;
 
-        while (game.getStatus()== GameState.LOGIN && game.getPlayers().size() < numberOfPlayersInt){
+        while (game.getStatus()== GameState.LOGIN && game.getPlayers().size() < numberOfPlayersInt){ // --> model in fase di LOGIN in attesa del primo player
 
-            System.out.println("Inserisci nickname: ");
-            String nickname = input.nextLine();
+            // --> quando si attiva il collegamento SOCKET/RMI con client viene richiesto nickname
+            System.out.println("Inserisci nickname: "); // --> inviato da Server a Client
+            String nickname = input.nextLine(); //inviato da Client a Server
 
-            TotemColor totemColor = askTotemColor(input, game); // sistemata scelta dei colori: un colore può essere scelto solo da uno, e questo viene mostrato agli altri
+            TotemColor totemColor = askTotemColor(input, game); //--> inviato da Server a Client richiesta di colore Totem, e ricevuta risposta
 
-            Player player = new Player(nickname, new Totem(totemColor));
+            // --> Server manda a tutti i client il nuovo player aggiunto (nickname e colore totem)
+
+            Player player = new Player(nickname, new Totem(totemColor)); // model effettivamente aggiunge player al gioco
             game.addPlayer(player);
 
             if(game.getPlayers().size() == 1){
-                System.out.println("Inserisci il numero di giocatori: ");
-                numberOfPlayersInt = Integer.parseInt(input.nextLine());
+                System.out.println("Inserisci il numero di giocatori: "); // --> se primo player aggiunto, richiesto numero giocatori
+                numberOfPlayersInt = Integer.parseInt(input.nextLine()); //--> ricevuta risposta, ora Model sa effettivamente quanti giocatori attendere, per ognuno richiede nickname e colore totem
             }
         }
 
+        //check players e totems
         System.out.println("Players: ");
         for(Player player : game.getPlayers()){
             System.out.println(player.getNickname() + " " + player.getTotem().getColor());
         }
 
-        game.startGame();
+        game.startGame(); //--> finita la fase di LOGIN, il model lancia startGame() e lo comunica a tutti i client
+        // --> in questa fase viene anche lanciata la grafica del gameBoard a tutti i client
+
 
         //simulazione round
-        for(int i = 1; i < 20; i++){
+        for(int i = 1; i < 11; i++){
 
-            if(game.getStatus()== GameState.END){
+            if(game.getStatus()== GameState.END){ //check extra
                 break;
             }
 
+            // --> ogni inizio round Server manda determinate informazioni a tutti i client: Numero round, Era??
             if(game.getCurrentAge()== Age.Last_Event){
                 System.out.println("\n--------------------------ROUND FINALE-------------------------\n");
             }else{
                 System.out.println("\n--------------------------round: " + i + "-------------------------\n");
             }
 
-
+            //qua le metto come stringhe, in realtà le player info e le game Board Info saranno poi nella view di ogni client, e andranno aggiornato per "ogni azione"
             System.out.println("players info:");
             for(Player player : game.getPlayers()){
                 System.out.println("\n" + player.toString());
             }
-
             System.out.println("\ngame board info:");
             System.out.println(game.getBoard().toString());
+
 
             System.out.println("\nGAME STATUS: " + game.getStatus()); //gamestate = OFFER_SPACE_CHOOSE
 
             while(game.getStatus() == GameState.OFFER_SPACE_CHOOSE){
-                System.out.println("\nPlayer " + game.getCurrentPlayer().getNickname() + " scegli il posto di offerte ");
+
+                System.out.println("\nPlayer " + game.getCurrentPlayer().getNickname() + " scegli il posto di offerte "); // --> Server manda a Client del player in turno la richiesta di scelta di posizione board
 
                 System.out.println("Inserisci lettera del posto: ");
-                char letter = input.nextLine().charAt(0);
+                char letter = input.nextLine().charAt(0);  // --> ricevuta risposta, Server sa il posto scelto dal client
 
                 boolean found = false;
+                // --> Server cerca il posto scelto e lo mette in offerto, se non lo trova, ritorna errore
                 for(BoardSpace bs : game.getBoard().getFreeBoardSpaces()){
                     if(bs.getLetter() == letter){
                         game.placeTotemOnOfferSpace(game.getCurrentPlayer(), bs);
@@ -92,14 +101,17 @@ public class MainGame {
                     System.out.println("Posto non disponibile");
                 }
 
+                // --> fino a che tutti i client si sono posizionati il server manda richiesta e rimane in attesa del player in turno, intanto altri client devono rimanere "fermi"
+                // --> però per ogni scelta fatta da un player (client) è importante che il server la comunichi a tutti gli altri client (scelta strategica di posizionamento)
             }
+
 
             System.out.println("\ngame board info:");
             System.out.println(game.getBoard().toString());
 
             System.out.println("\nGAME STATUS: " + game.getStatus()); //gamestate = PICKING_CARD
 
-            while(game.getStatus() == GameState.PICKING_CARD){
+            while(game.getStatus() == GameState.PICKING_CARD){ //--> la logica è la stessa per la scleta del board space: server manda richiesta al client in turno, client risponde con le varie pick card, anche tutti gli altri client vengono aggiornati (devono sapere che carte sono state prese)
                 String playerNickname = game.getCurrentPlayer().getNickname();
 
                 System.out.println("\nPlayer " + playerNickname + " scegli carta (inserisci id):");
@@ -134,7 +146,7 @@ public class MainGame {
 
                 System.out.println("prossima carta");
 
-
+                // --> attenziona ad aggiornare corrente la view del gameBoard per ogni client, per ogni carta pescata
             }
 
             System.out.println("players info:");
@@ -151,7 +163,7 @@ public class MainGame {
 
         }
 
-        if(game.getStatus() == GameState.END){
+        if(game.getStatus() == GameState.END){ //--> comunicazione a tutti i client dei risultati, particolare comunicazione per il vincitore
 
             System.out.println("\n\nPartita terminata, ecco i risultati:\n");
 
