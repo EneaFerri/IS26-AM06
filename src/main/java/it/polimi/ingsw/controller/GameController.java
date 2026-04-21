7c9cf31af76704cdff332f4afa0531a2f6707d11
Enea Ferri <enea.ferri@mail.polimi.it>
2026-04-21 13:14:09 +0200

    piccoli bug risolti

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -1,7 +1,8 @@
-package it.polimi.ingsw.network.rmi_v0.server;
+package it.polimi.ingsw.controller;
 
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.board.BoardSpace;
+import it.polimi.ingsw.model.cards.Card;
 import it.polimi.ingsw.model.player.Player;
 
 public class GameController {
@@ -12,13 +13,6 @@ public class GameController {
         this.game = game;
     }
 
-    private Player getPlayer(String nickname) {
-        return game.getPlayers().stream()
-                .filter(p -> p.getNickname().equals(nickname))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Player not found"));
-    }
-
     public void placeTotem(String nickname, char spaceLetter) {
         Player p = getPlayer(nickname);
         BoardSpace space = game.getBoard().getBoardSpace(spaceLetter);
@@ -26,21 +20,30 @@ public class GameController {
         game.placeTotemOnOfferSpace(p, space);
     }
 
-    public void pickTopCard(String nickname, int index) {
+    public void pickCard(String nickname, int cardId) {
         Player p = getPlayer(nickname);
+        Card c = getCard(cardId);
 
-        var cards = game.getBoard().getAvailableUpperTribeCards();
-        game.pickCard(p, cards.get(index));
-    }
+        game.pickCard(p,c);
 
-    public void pickBottomCard(String nickname, int index) {
-        Player p = getPlayer(nickname);
-
-        var cards = game.getBoard().getAvailableBottomTribeCards();
-        game.pickCard(p, cards.get(index));
     }
 
     public void startGame() {
         game.startGame();
+    }
+
+    //internal methods
+    private Player getPlayer(String nickname) {
+        return game.getPlayers().stream()
+                .filter(p -> p.getNickname().equals(nickname))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Player not found"));
+    }
+
+    private Card getCard(int cardId){
+        return game.getMainDeck().getAllCards().stream()
+                .filter(c-> c.getID() == cardId)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Card not found"));
     }
 }
--- src/main/java/it/polimi/ingsw/network/rmi_v0/server/RmiServer.java
+++ src/main/java/it/polimi/ingsw/network/rmi_v0/server/RmiServer.java
@@ -1,11 +1,13 @@
 package it.polimi.ingsw.network.rmi_v0.server;
 
+import it.polimi.ingsw.controller.GameController;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.enums.TotemColor;
 import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.model.player.Totem;
-import it.polimi.ingsw.network.common.GameView;
+
 import it.polimi.ingsw.network.rmi_v0.client.VirtualServerRmi;
+import it.polimi.ingsw.network.rmi_v0.common.GameView;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -15,14 +17,14 @@ import java.util.*;
 
 public class RmiServer extends UnicastRemoteObject implements VirtualServerRmi {
 
-    private final it.polimi.ingsw.network.rmi_v0.server.GameController controller;
+    private final GameController controller;
     private final Game game;
 
     private final Map<String, VirtualViewRmi> clients = new HashMap<>();
 
     public RmiServer() throws RemoteException {
         this.game = new Game(1);
-        this.controller = new it.polimi.ingsw.network.rmi_v0.server.GameController(game);
+        this.controller = new GameController(game);
     }
 
     public static void main(String[] args) throws Exception {
