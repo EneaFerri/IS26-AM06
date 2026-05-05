fc20b6e536686cfd25e62cde04953c4db88e2314
FiloFiandra <filippo.fiandra@mail.polimi.it>
2026-05-05 16:54:20 +0200

    Modifica IP

--- src/main/java/it/polimi/ingsw/CombinedServer.java
+++ src/main/java/it/polimi/ingsw/CombinedServer.java
@@ -20,7 +20,7 @@ import java.rmi.RemoteException;
 public class CombinedServer {
 
     public static void main(String[] args) {
-        String ip = NetworkUtils.resolveLocalIp();
+        String ip = "localhost";
         System.setProperty("java.rmi.server.hostname", ip);
 
         LobbyManager sharedLobbyManager = new LobbyManager();
